8cfd354bbae9bf56d44087890d541773e5f0521f
vijrahul5 <43812609+vijrahul5@users.noreply.github.com>
2021-06-26 15:59:12 +0530

    Delete secrets.js

--- config/secrets.js
+++
@@ -1,3 +0,0 @@
-module.exports.DB_LINK = "mongodb+srv://admin:rahul12345@cluster0-hobya.mongodb.net/test?retryWrites=true&w=majority";
-module.exports.JWT_SECRET = "tchjsksjbdjhdkwo1634jf83h2h2nnnfjr3333";
-module.exports.APP_PASSWORD = "Championalltime1";
